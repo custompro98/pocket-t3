9c916af89f5fc7f315b5ce91e31dad96fc07c15d
custompro98 <mitchjoa@gmail.com>
2022-11-12 13:55:18 -0500

    rm example trpc router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,9 @@
 // src/server/trpc/router/_app.ts
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
 import { bookmarkRouter } from "./bookmark";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   bookmark: bookmarkRouter,
 });
